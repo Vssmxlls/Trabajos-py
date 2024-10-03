f2f92d7af27a175152bfb7e416b4e2171b7a6a15
Vsmxlls <sarangocu@unadvirtual.edu.co>
2024-10-03 17:02:13 -0500

    Mejorado

--- AlmacenVisteconEstilo.py
+++ AlmacenVisteconEstilo.py
@@ -2,12 +2,19 @@
 print ("Sea bienvenido a Almacenes Viste con estilo")
 estatura = int(input("Por facor ingrese su estatura en centimetros: "))
 
-
 if estatura <=150:
-    print(" Su talla de ropa segun su estatura es: S")
+    print("Su talla de ropa respecto a su altura es de: S")
+    
+elif (150 > estatura <170):
+        # comment: 
+        print("Su talla de ropa respecto a su altura es de: M")
+
+elif (170 >= estatura <180):
+    # comment: 
+    print("Su talla de ropa respecto a su altura es de: L")
+
+if estatura >=180:
+    print("Su talla de ropa respecto a su altura es de: XL")
 
 
-elif (estatura >150 <170):
-
- print("Su talla de ropa segun su estatura es: M")
 
