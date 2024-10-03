a862d4a395554e17ab96ded19a76ec7210a8f978
Vsmxlls <sarangocu@unadvirtual.edu.co>
2024-10-03 15:27:29 -0500

    Changes

--- AlmacenVisteconEstilo.py
+++ AlmacenVisteconEstilo.py
@@ -1,3 +1,12 @@
 #Programa para Talla de ropa, segun tu estatura#
 print ("Sea bienvenido a Almacenes Viste con estilo")
 estatura = int(input("Por facor ingrese su estatura en centimetros: "))
+
+
+if estatura <=150:
+    print(" Su talla de ropa segun su estatura es: S")
+
+if estatura  >150:
+    print("Su talla de ropa segun su estatura es: M")
+
+    
