1da8ef8535dd97d332d9ec1acb5c23eeb84e4c3e
Vsmxlls <sarangocu@unadvirtual.edu.co>
2024-11-05 17:40:09 -0500

    sisisisis

--- Creando Programas con ciclos/EmpresaByte.py
+++ Creando Programas con ciclos/EmpresaByte.py
@@ -18,7 +18,7 @@ for i in range (ventas):
     contador_portatil +=1
     descuento = valor_articulo * 0.15
     valor_final = valor_articulo - descuento
-    print("El valor final a pagar es de: ", valor_final)
+    print("El valor final a pagar es de: ", valor_final, "pesos")
    
  elif (opcion == 2):
     # comment: 
@@ -26,6 +26,15 @@ for i in range (ventas):
     descuento2 = valor_articulo * 0.05
     valor_final2 = valor_articulo - descuento2
     print("El valor final a pagar es de: ", valor_final2, "pesos")
+
+ elif (opcion == 3):
+    contador_otros_articulos +=1
+    descuento3 = valor_articulo * 0.02
+    valor_final3 = valor_articulo - descuento3
+    print("El valor final a pagar es de: ", valor_final3, "pesos")
+
+
+        
 
 
 
