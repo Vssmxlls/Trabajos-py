a9d644fab272e2794f0b7b20b2e4960f65512fa1
Vsmxlls <sarangocu@unadvirtual.edu.co>
2024-11-05 17:18:43 -0500

    sizas

---
+++ Creando Programas con ciclos/EmpresaByte.py
@@ -0,0 +1,30 @@
+contador_portatil = 0
+contador_camaras = 0
+contador_otros_articulos = 0
+
+ventas = 25
+for i in range (ventas):
+    # comment: 
+
+ print("Bienvenido a la empresa byte, por favor revise su articulo a comprar")
+ print("1. Linea de portatiles")
+ print("2. Linea de Camaras")
+ print("3. Otros Articulos")
+ opcion = input("Ingrese por favor el numero del articulo, de acuerdo al menu: ")
+ valor_articulo = int(input("Ingrese por favor el valor del articulo: "))
+  if (opcion ==1):
+    # comment: 
+    contador_portatil +=1
+    descuento = valor_articulo * 0.15
+    valor_final = valor_articulo - descuento
+    print("El valor final a pagar es de: ", valor_final)
+   # end if
+
+
+
+
+
+    
+ 
+   
+
