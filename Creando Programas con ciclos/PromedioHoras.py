7eb53726da5e64c60d50a29f55603989844a7146
Vsmxlls <sarangocu@unadvirtual.edu.co>
2024-11-06 13:42:08 -0500

    SUJJJKHG

---
+++ Creando Programas con ciclos/PromedioHoras.py
@@ -0,0 +1,8 @@
+horas_personas = []
+personas_encuestadas = 15
+horas = int(input("Ingrese por favor el numero de horas que gasta en promedio los dias domingos: "))
+
+while (personas_encuestadas ):
+    # comment: 
+ horas_personas.append(horas)
+# end while
