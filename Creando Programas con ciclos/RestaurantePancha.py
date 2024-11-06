8f922bcaf704ade5e09f49adc807bcb8de9481f0
Vsmxlls <sarangocu@unadvirtual.edu.co>
2024-11-05 19:17:41 -0500

    si

---
+++ Creando Programas con ciclos/RestaurantePancha.py
@@ -0,0 +1,11 @@
+while (True):
+    # comment:
+    valor_cuenta = int(input("Ingrese el valor de la cuenta a pagar del cliente: "))
+    propina_sugerida = valor_cuenta * 0.15
+    print("El valor de la propina sugerida es de: ", propina_sugerida, "pesos")
+
+    respuesta = input("¿Desea registrar otro cliente?: ")
+
+    if respuesta = si:
+        print("Registro terminado")
+        break
