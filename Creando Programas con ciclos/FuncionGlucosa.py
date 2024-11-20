c3e7006ecf44d8c2dfc36fe1103e2592307eaaa3
Vsmxlls <sarangocu@unadvirtual.edu.co>
2024-11-20 17:33:59 -0500

    si

---
+++ Creando Programas con ciclos/FuncionGlucosa.py
@@ -0,0 +1,104 @@
+def clasificacion_de_glucosa(nivel):
+    if nivel <= 75:
+        return "Hipoglucemia"  
+    elif 75 < nivel < 105:
+        return "Normal" 
+    elif 105 <= nivel < 126:
+        return "Prediabetes" 
+    else:
+        return "Diabetes"  
+
+nivel = int(input("Por favor ingrese el nivel de glucosa del paciente en ml: "))
+resultado = clasificacion_de_glucosa(nivel)
+print("Su clasificación de glucosa de acuerdo a su nivel es:", resultado)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+ 
+
+ 
+   
