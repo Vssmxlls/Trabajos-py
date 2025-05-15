fd2c1bae69e521b65b8a5ca235e8f4adf25f9907
Vssmxlls <samuelcuesta56782094@gmail.com>
2025-05-15 16:43:40 -0500

    Creacion del servidor

--- Cliente Y Servidor - Final/Servidor.py
+++ Cliente Y Servidor - Final/Servidor.py
@@ -1,1 +1,17 @@
 import socket 
+import threading
+HOST = '127.0.0.1'
+PORT = 65432
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as socket:
+    socket.bind((HOST,PORT))
+    socket.listen()
+
+    conn, addr = socket.accept()
+
+    with conn:
+        print(f"Conexion exitosa con el cliente",{addr})
+        while True:
+            data = conn.recv(1204)
+            if not data: break
+            conn.sendall(data)
